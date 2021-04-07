cb9ef3c63c49a553a5f6c89b2fe4b0e888dd277b
Boguslaw Wiacek <bmw@parasoft.com>
2021-04-07 13:55:14 +0200

    Original files

---
+++ pre_test.cxx
@@ -0,0 +1,12 @@
+/*
+ * pre_test.cxx
+ *
+ *  Created on: 11 Mar 2021
+ *      Author: bmw
+ */
+
+
+#define PI 3.14                             // Violation
+
+#define SUM(A,B) ((A)+(B))                  // Violation
+
